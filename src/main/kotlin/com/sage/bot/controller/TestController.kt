24f62e8319ae22743389306087d7bfe79ce37847
Stanislav Laptenkov <laptenkovsy@bcs.ru>
2023-03-21 23:25:59 +0700

    BOT-000001 [MINOR] Add heroku procfile.

---
+++ src/main/kotlin/com/sage/bot/controller/TestController.kt
@@ -0,0 +1,11 @@
+package com.sage.bot.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/{name}")
+    fun get(@PathVariable name: String) = "Hello, $name"
+}
