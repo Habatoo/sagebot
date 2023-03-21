dbe36bdf0321d3eb3ee3c3f3b95ebd54e8564dcf
Stanislav Laptenkov <laptenkovsy@bcs.ru>
2023-03-21 22:40:19 +0700

    initial commit

---
+++ src/test/kotlin/com/sage/bot/SagebotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sage.bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SagebotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
