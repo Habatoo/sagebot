8ff2d85cbe2431e1fef789ee0f96747a2e8af43a
Stanislav Laptenkov <laptenkovsy@bcs.ru>
2023-03-26 14:40:35 +0700

    BOT-000009 [MINOR] Add log.

--- src/main/kotlin/com/sage/bot/service/command/ServiceCommand.kt
+++ src/main/kotlin/com/sage/bot/service/command/ServiceCommand.kt
@@ -1,5 +1,6 @@
 package com.sage.bot.service.command
 
+import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.bots.AbsSender
@@ -17,6 +18,7 @@ abstract class ServiceCommand(
     description,
 ) {
 
+    private val log = LoggerFactory.getLogger(ServiceCommand::class.java)
     /**
      * Отправка ответа пользователю
      */
@@ -28,6 +30,7 @@ abstract class ServiceCommand(
         try {
             absSender.execute(message)
         } catch (e: TelegramApiException) {
+            log.error("ServiceCommand error ${e.message}")
             e.printStackTrace()
         }
     }
